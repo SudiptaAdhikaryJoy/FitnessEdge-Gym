0c1e515a6c5e1bff7c00938842a7caf79b455a9b
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-10-19 01:59:14 +0600

    add files and edited

---
+++ src/Pages/About/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import fitness from '../../images/about/fitness-1.jpeg';
+import './About.css';
+const About = () => {
+    return (
+        <div className="main-container mt-5">
+                <h2>Know us</h2>
+            <div className="container gap-5">
+                <div className="img-container">
+                    <img className="img-fluid p-3 mx-0" src={fitness} alt="" />
+                </div>
+                <div className="text-container">
+                    <h2>Be Self Dependent</h2>
+                    <p className="text">At Gym and Fitness, we believe that building a positive culture is incredibly important. The business is underpinned by six core values which speak of our commitment to our customers, staff, the industry and our business as a whole. We believe in encouraging, supporting, challenging, learning and growing to be the best. At Gym and Fitness, every morsel of feedback is a nugget of shiny gold because it gives us the opportunity to provide the best customer experience.</p>
+
+                    <button className="btn-regular">know Us</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
